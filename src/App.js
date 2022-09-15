e3140aedf64e84d8bec122720c8e82cafc88c583
Kai Takami <kaitakami@protonmail.com>
2022-09-15 22:17:34 +0900

    Finish select category page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
 import { useState } from "react";
+import Message from "./components/Message";
+import Quiz from "./components/Quiz";
 import GameStart from "./components/GameStart";
 
 function App() {
   const [isGameStarted, setIsGameStarted] = useState(false);
-  const [selectedCategory, setSelectedCategory] = useState({})
+  const [selectedCategory, setSelectedCategory] = useState({});
+  const [displayingError, setDisplayingError] = useState(false);
 
-  function handleCategoryChange (category) {
-    // setSelectedCategory
-    console.log(category)
+  function handleCategoryChange(category) {
+    setSelectedCategory(category);
+  }
+
+  function startGame() {
+    // validate if a category is selected
+    if (Object.keys(selectedCategory).length > 0) {
+      setIsGameStarted(true);
+      setDisplayingError(false);
+    } else {
+      setDisplayingError(true);
+      // The time should be the same to the animation applied to the classname message in index.css
+      setTimeout(() => setDisplayingError(false), 5000);
+    }
   }
 
   return (
     <div className="container">
-      {isGameStarted ? null : <GameStart setIsGameStarted={setIsGameStarted} handleCategoryChange={handleCategoryChange} />}
+      {displayingError && (
+        <Message children={"Select a category to get started"} />
+      )}
+      {isGameStarted ? (
+        <Quiz category={selectedCategory} />
+      ) : (
+        <GameStart
+          handleCategoryChange={handleCategoryChange}
+          selectedCategory={selectedCategory}
+          startGame={startGame}
+        />
+      )}
     </div>
   );
 }
